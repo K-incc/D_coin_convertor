16f25b7d49b736cbd34cac89ef2ebf7a48a5dadb
kinc <kinc.developer@gmail.com>
2020-04-21 23:45:21 +0300

    Updates

---
+++ precache-manifest.c469546a58c84c16fa1ea39b38c66c0f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b0e8085022d94ad71e898aa01e4dba6",
+    "url": "/currency_convertor/index.html"
+  },
+  {
+    "revision": "5d35c6a454e57b68c440",
+    "url": "/currency_convertor/static/js/2.a137fc94.chunk.js"
+  },
+  {
+    "revision": "79d57e8284b57ed34639",
+    "url": "/currency_convertor/static/js/main.3e5b7ecd.chunk.js"
+  },
+  {
+    "revision": "4b951a00a181f8b8e23a",
+    "url": "/currency_convertor/static/js/runtime-main.d8674ea1.js"
+  }
+]);
