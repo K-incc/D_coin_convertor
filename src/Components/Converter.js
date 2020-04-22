b7a124fdce965e386972858461027afe7f291f7a
kinc <kinc.developer@gmail.com>
2020-04-22 21:50:33 +0300

    Action test commit

--- src/Components/Converter.js
+++ src/Components/Converter.js
@@ -115,7 +115,7 @@ const Converter=()=>{
 							{data.result} {data.convertto}
 						</Typography>
 						<Typography variant="subtitle1">
-							Ass of {data.date}
+							Asss of {data.date}
 						</Typography>
 	        
 	        			<div className={classes.roott}>
